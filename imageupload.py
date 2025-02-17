33226f944f99ae65fe079850d9e15dcb1ea8ca30
NasywaKml <153457446+NasywaKml@users.noreply.github.com>
2025-02-17 21:50:51 +0700

    Update imageupload.py

--- imageupload.py
+++ imageupload.py
@@ -11,7 +11,8 @@ from ultralytics import YOLO
 # Explicitly specify YOLOv5 local path
 if not os.path.exists("best.pt"):
     model_download_id = "1EiG6pPnhVokLRBwUt9rNk6FhsvkUv1ri"
-    gdown.download(f'https://drive.google.com/uc?id={model_download_id}', f’best.pt’, quiet=False)
+    gdown.download(f'https://drive.google.com/uc?id={model_download_id}', f'best.pt', quiet=False)
+
 
 model = YOLO("best.pt")
 
